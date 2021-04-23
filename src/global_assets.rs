356d7a983b8e51c6c37fcef1da572478c2a367a9
Darcy Ryan <darcy150@gmail.com>
2021-04-24 08:37:42 +1000

    Add chess board and piece assets

---
+++ src/global_assets.rs
@@ -0,0 +1,104 @@
+use bevy::prelude::*;
+
+#[derive(Debug, PartialEq)]
+pub enum GlobalMesh {
+    Tile,
+    King,
+    KingCross,
+    Pawn,
+    Knight1,
+    Knight2,
+    Rook,
+    Bishop,
+    Queen,
+}
+use GlobalMesh::*;
+
+#[derive(Debug, PartialEq)]
+pub enum GlobalMaterial {
+    BlackTile,
+    WhiteTile,
+    BlackPiece,
+    WhitePiece,
+}
+use GlobalMaterial::*;
+
+pub struct GlobalAssets {
+    pub mesh_tile: Handle<Mesh>, 
+
+    pub mesh_king: Handle<Mesh>,
+    pub mesh_king_cross: Handle<Mesh>,
+    pub mesh_pawn: Handle<Mesh>,
+    pub mesh_knight_1: Handle<Mesh>,
+    pub mesh_knight_2: Handle<Mesh>,
+    pub mesh_rook: Handle<Mesh>,
+    pub mesh_bishop: Handle<Mesh>,
+    pub mesh_queen: Handle<Mesh>,
+
+    pub mat_black_tile: Handle<StandardMaterial>,
+    pub mat_white_tile: Handle<StandardMaterial>,
+    
+    pub mat_black_piece: Handle<StandardMaterial>,
+    pub mat_white_piece: Handle<StandardMaterial>,
+}
+
+impl GlobalAssets {
+    pub fn clone_mesh(&self, mesh: GlobalMesh) -> Handle<Mesh> {
+        match mesh {
+            Tile      => self.mesh_tile.clone(),
+            King      => self.mesh_king.clone(),
+            KingCross => self.mesh_king_cross.clone(),
+            Pawn      => self.mesh_pawn.clone(),
+            Knight1   => self.mesh_knight_1.clone(),
+            Knight2   => self.mesh_knight_2.clone(),
+            Rook      => self.mesh_rook.clone(),
+            Bishop    => self.mesh_bishop.clone(),
+            Queen     => self.mesh_queen.clone(),
+        }
+    }
+
+    pub fn clone_material(&self, material: GlobalMaterial) -> Handle<StandardMaterial> {
+        match material {
+            BlackTile  => self.mat_black_tile.clone(),
+            WhiteTile  => self.mat_white_tile.clone(),
+            BlackPiece => self.mat_black_piece.clone(),
+            WhitePiece => self.mat_white_piece.clone(),
+        }
+    }
+}
+
+pub struct GlobalAssetsPlugin;
+
+impl GlobalAssetsPlugin {
+    fn setup(
+        mut cmd:          Commands,
+        mut meshes:       ResMut<Assets<Mesh>>,
+        mut materials:    ResMut<Assets<StandardMaterial>>,
+            asset_server: Res<AssetServer>,
+    ) {
+        cmd.insert_resource(GlobalAssets {
+            mesh_tile: meshes.add(Mesh::from(shape::Plane { size: 1.0 })),
+
+            mesh_king:       asset_server.load("models/chess_kit/pieces.glb#Mesh0/Primitive0"),
+            mesh_king_cross: asset_server.load("models/chess_kit/pieces.glb#Mesh1/Primitive0"),
+            mesh_pawn:       asset_server.load("models/chess_kit/pieces.glb#Mesh2/Primitive0"),
+            mesh_knight_1:   asset_server.load("models/chess_kit/pieces.glb#Mesh3/Primitive0"),
+            mesh_knight_2:   asset_server.load("models/chess_kit/pieces.glb#Mesh4/Primitive0"),
+            mesh_rook:       asset_server.load("models/chess_kit/pieces.glb#Mesh5/Primitive0"),
+            mesh_bishop:     asset_server.load("models/chess_kit/pieces.glb#Mesh6/Primitive0"),
+            mesh_queen:      asset_server.load("models/chess_kit/pieces.glb#Mesh7/Primitive0"),
+
+            mat_black_tile: materials.add(Color::rgb(1., 0.9, 0.9).into()),
+            mat_white_tile: materials.add(Color::rgb(0., 0.1, 0.1).into()),
+
+            mat_black_piece:  materials.add(Color::rgb(0., 0.2, 0.2).into()),
+            mat_white_piece:  materials.add(Color::rgb(1., 0.8, 0.8).into()),
+        })
+    }
+}
+
+impl Plugin for GlobalAssetsPlugin {
+    fn build(&self, app: &mut AppBuilder) {
+        app.add_startup_system_to_stage(StartupStage::PreStartup, Self::setup.system());
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,77 @@
 use bevy::prelude::*;
+use global_assets::{GlobalAssets, GlobalAssetsPlugin};
+
+mod global_assets;
 
 fn main() {
     App::build()
+        .insert_resource(Msaa { samples: 4})
+        .insert_resource(WindowDescriptor {
+            title: "Chess!".to_string(),
+            width: 1600.,
+            height: 1600.,
+            ..Default::default()
+        })
         .add_plugins(DefaultPlugins)
+        .add_plugin(GlobalAssetsPlugin)
+        .add_startup_system(setup.system())
+        .add_startup_system(create_board.system())
         .run();
+}
+
+fn setup (
+    mut cmd: Commands,
+) {
+    
+    let cam_pos   = Vec3::new(-7., 20., 4.);
+    let light_pos = Vec3::new(4., 8., 4.);
+
+    let cam_rot = Quat::from_xyzw(-0.3, -0.5, -0.3, 0.5).normalize();
+    
+    // Camera
+    cmd
+        .spawn_bundle(PerspectiveCameraBundle {
+            transform: 
+            Transform {
+                translation: cam_pos,
+                rotation: cam_rot,
+                ..Default::default()
+            },
+            ..Default::default()
+        });
+    
+    // Light
+    cmd
+        .spawn_bundle(LightBundle {
+            transform: Transform {
+                translation: light_pos,
+                ..Default::default()
+            },
+            ..Default::default()
+        });
+}
+
+fn create_board (
+    mut cmd: Commands,
+        global_assets: Res<GlobalAssets>,
+) {
+
+    for i in 0..8 {
+        for j in 0..8 {
+            let transform = Transform::from_translation(Vec3::new(i as f32, 0., j as f32));
+            let mesh = global_assets.mesh_tile.clone();
+            let material: Handle<StandardMaterial>;
+
+            if (i + j + 1) % 2 == 0 {
+                material = global_assets.mat_white_tile.clone();
+            } else {
+                material = global_assets.mat_black_tile.clone();
+            }
+
+            cmd
+                .spawn_bundle(PbrBundle { transform, mesh, material, ..Default::default() });
+
+        }
+    }
+
 }
